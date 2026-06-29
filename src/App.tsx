727a64664645e14f8299c5d6b34fc42d596ac561
ryan <ryancruse1807@gmail.com>
2026-06-29 15:02:52 +0100

    chore: Configure typography and design system

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,16 @@
 
 function App() {
   return (
-    <h1 className="text-5xl">
-      Test
-    </h1>
+    <>
+      <h1 className="text-5xl">
+        Test
+      </h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati dolores nihil quidem ratione atque expedita fugit! Cumque velit dignissimos esse.</p>
+    </>
+
+
+
   )
 }
 
